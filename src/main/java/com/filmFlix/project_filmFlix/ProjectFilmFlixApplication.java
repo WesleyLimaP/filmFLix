e7846c9974cf831a9e6eee32e7516046066f1962
wesley <wesleylima029@gmail.com>
2025-03-27 20:41:12 -0300

    project add

---
+++ src/main/java/com/filmFlix/project_filmFlix/ProjectFilmFlixApplication.java
@@ -0,0 +1,13 @@
+package com.filmFlix.project_filmFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFilmFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFilmFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filmFlix/project_filmFlix/ProjectFilmFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmFlix.project_filmFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFilmFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
